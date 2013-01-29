3574d120d1bc7d254e58dfc49e2bf45b1f5c0f15
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-01-29 18:59:11 +0000

    automatically detect bjam bin dir
    - incompatible change: tgen's with 'bjam' feature need a 'boost_libs' attribute
    - use the algorhithm used by boost to detect bjam bin folder
    - remove some debug ptrints

--- bjam.py
+++ bjam.py
@@ -7,10 +7,49 @@ from waflib import Logs
 from waflib.TaskGen import feature, after_method
 from waflib.Task import Task, always_run
 
+# from boost source code:
+def _get_bjam_build_dir():
+    import os
+    if os.name == 'nt':
+        return "bin.ntx86"
+    elif (os.name == 'posix') and os.__dict__.has_key('uname'):
+        if os.uname()[0].lower().startswith('cygwin'):
+            return "bin.cygwinx86"
+            if 'TMP' in os.environ and os.environ['TMP'].find('~') != -1:
+                print 'Setting $TMP to /tmp to get around problem with short path names'
+                os.environ['TMP'] = '/tmp'
+        elif os.uname()[0] == 'Linux':
+            cpu = os.uname()[4]
+            if re.match("i.86", cpu):
+                return "bin.linuxx86";
+            else:
+                return "bin.linux" + os.uname()[4]
+        elif os.uname()[0] == 'SunOS':
+            return "bin.solaris"
+        elif os.uname()[0] == 'Darwin':
+            if os.uname()[4] == 'i386':
+                return "bin.macosxx86"
+            else:
+                return "bin.macosxppc"
+        elif os.uname()[0] == "AIX":
+            return "bin.aix"
+        elif os.uname()[0] == "IRIX64":
+            return "bin.irix"
+        elif os.uname()[0] == "FreeBSD":
+            return "bin.freebsd"
+        elif os.uname()[0] == "OSF1":
+            return "bin.osf"
+        else:
+            Logs.warn( "Don't know directory where Jam is built for this system: " + os.name + "/" + os.uname()[0] )
+            return None
+    else:
+        Logs.warn( "Don't know directory where Jam is built for this system: " + os.name )
+        return None
+
 def options(opt):
     grp = opt.add_option_group('Bjam Options')
-    grp.add_option('--bjam_src', default=None, help='You can find it in <boost root>/tools/jam/src')
-    grp.add_option('--bjam_uname', default='linuxx86_64', help='bjam is built in <src>/bin.<uname>/bjam')
+    grp.add_option('--bjam_src', default=None, help='You can find it in <boost root>/tools/build/v2')
+    grp.add_option('--bjam_uname', default=_get_bjam_build_dir(), help='bjam is built in <src>/engine/<uname>')
     grp.add_option('--bjam_config', default=None)
     grp.add_option('--bjam_toolset', default=None)
 
@@ -21,7 +60,7 @@ def configure(cnf):
         cnf.env.BJAM_UNAME = cnf.options.bjam_uname
     try:
         cnf.find_program('bjam', path_list=[
-            cnf.env.BJAM_SRC + sep + 'bin.' + cnf.env.BJAM_UNAME
+            cnf.env.BJAM_SRC + sep + 'engine' + sep + cnf.env.BJAM_UNAME
         ])
     except Exception as e:
         cnf.env.BJAM = None
@@ -44,7 +83,7 @@ def process_bjam(self):
 
 class bjam_creator(Task):
     ext_out = 'bjam_exe'
-    before = ['bjam_build']
+    before = ['bjam_build', 'cxxprogram' ]
     vars=['BJAM_SRC', 'BJAM_UNAME']
     def run(self):
         env = self.env
@@ -54,26 +93,30 @@ class bjam_creator(Task):
         if not env.BJAM_SRC:
             Logs.error('bjam needs to be built; please re-configure the project providing the location of bjam sources with the --bjam_src option.')
             return -1
-        bjam = gen.bld.root.find_dir(env.BJAM_SRC)
+        bjam = ''
+        try:
+            bjam = gen.bld.root.find_dir(env.BJAM_SRC)
+        except:
+            Logs.error( 'Cannot find folder: %s' % env.BJAM_SRC )
+            return -1
         if not bjam:
             Logs.error('Can not find bjam source')
             return -1
-        bjam_exe_relpath = 'bin.' + env.BJAM_UNAME + '/bjam'
-        bjam_exe = bjam.find_resource(bjam_exe_relpath)
-        if bjam_exe:
-            Logs.warn( 'bjam executable found: %s' % bjam_exe.abspath() )
-            env.BJAM = bjam_exe.srcpath()
-            return 0
+        if not bjam.find_resource( 'bootstrap.bat' ) and bjam.find_resource( 'bootstrap.sh' ):
+            Logs.error( 'bjam source folder must contain bootstrap.bat and bootstrap.sh, please make sure to provide the correct folder with the --bjam_src option.' )
+            return -1
         if name == 'nt':
             bjam_cmd = ['cmd', '/c', 'bootstrap.bat']
+            bjam_exe_name = 'bjam.exe'
         else:
             bjam_cmd = ['bootstrap.sh']
+            bjam_exe_name = 'bjam'
         Logs.warn( 'Building bjam: %s' % str(bjam_cmd) )
         result = self.exec_command(bjam_cmd, cwd=bjam.abspath())
         if not( result == 0 ):
             Logs.error('bjam bootstrap failed (non-zero return value)')
             return -1
-        bjam_exe = bjam.find_resource(bjam_exe_relpath)
+        bjam_exe = bjam.find_resource( 'engine/' + env.BJAM_UNAME + '/' + bjam_exe_name )
         if bjam_exe:
             env.BJAM = bjam_exe.srcpath()
             return 0
@@ -104,17 +147,16 @@ class bjam_build(Task):
         if not bjam_exe:
             Logs.error('env.BJAM is not set')
             return -1
-        bjam_exe_rel = bjam_exe.relpath_gen(build_root)
+        bjam_exe_rel = bjam_exe.path_from(build_root)
         cmd = ([bjam_exe_rel] +
             (['--user-config=' + jam_rel] if jam_rel else []) +
             ['--stagedir=' + path.get_bld().path_from(build_root)] +
             ['--debug-configuration'] +
-            ['--with-' + lib for lib in self.generator.target] +
+            ['--with-' + lib for lib in self.generator.boost_libs] +
             (['toolset=' + env.BJAM_TOOLSET] if env.BJAM_TOOLSET else []) +
             ['link=' + 'shared'] +
             ['variant=' + 'release']
         )
-        Logs.debug('runner: ' + build_root.srcpath() + '> ' + str(cmd))
         ret = self.exec_command(cmd, cwd=build_root.srcpath())
         if ret != 0:
             return ret
